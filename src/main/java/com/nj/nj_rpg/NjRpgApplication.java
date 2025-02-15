7b9d3c1d3feb90c8b81a4e700a3b2de06ce14ddc
NyorJa <botsot.felix@gmail.com>
2025-02-15 11:36:43 +0800

    Initial commit

---
+++ src/main/java/com/nj/nj_rpg/NjRpgApplication.java
@@ -0,0 +1,13 @@
+package com.nj.nj_rpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nj/nj_rpg/NjRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nj.nj_rpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
